bf1cd0b8b4208572b92079e21a0c0aca4f526bd4
Christian Johannes <cajiky@gmail.com>
2023-04-04 18:37:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import { lightTheme, darkTheme } from "./styles/themes";
+import React, { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const appliedTheme = theme === "light" ? lightTheme : darkTheme;
+  console.log("Applied theme:", appliedTheme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={appliedTheme}>
+      <CssBaseline />
+      <Typography variant="p">Hello, this is paragraph Typography!</Typography>
+      <Typography variant="h3">Hello, this is a h3 Typography!</Typography>
+      <Typography variant="h2">Hello, this is h2 Typography!</Typography>
+      <Typography variant="h1">Hello, this is h1 Typography!</Typography>
+      {/* Your app content here */}
+    </ThemeProvider>
   );
 }
 
